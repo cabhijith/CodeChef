898c315a8f3f4929caa060f1f610607cb799fa30
Abhijith Chandran <45108441+cabhijith@users.noreply.github.com>
2019-11-21 15:21:45 +0530

    SUPW CodeChef

---
+++ SUPW.cpp
@@ -0,0 +1,23 @@
+//Link to question: https://www.codechef.com/ZCOPRAC/problems/ZCO14002
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+int main() {
+	// your code goes here
+	int n;
+  cin >> n;
+  int arr[n];
+  int dp[n];
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  dp[0] = arr[0];
+  dp[1] = arr[1];
+  dp[2] = arr[2];
+  for(int i = 3; i < n; i++){
+    dp[i] = arr[i] + min({dp[i-1],dp[i -2], dp[i - 3]});
+  }
+  cout << min({dp[n-1], dp[n-2], dp[n-3]});
+  }
+
